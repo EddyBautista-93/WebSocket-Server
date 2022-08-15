8bc42d764a4c183ff3a848b7259b187d7591e0d9
EddyBautista <bautista.j.eddy@gmail.com>
2022-08-15 06:44:53 -0500

    set up server

---
+++ Server.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Net;
+
+namespace WebSocket_Server
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 80);
+            server.Start();
+            Console.WriteLine("Server has started on 127.0.0.1:80.{0}Waiting for a connection…", Environment.NewLine);
+            TcpClient client = server.AcceptTcpClient();
+
+            Console.WriteLine("A client connected");
+        }
+    }
+}
